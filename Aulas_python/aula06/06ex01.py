693b90c9c3fd7c073e8bfd9b95989fa89ea52e75
Junior <fabjuniorcc@gmail.com>
2025-02-03 12:52:12 -0300

    sessions

---
+++ Aulas_python/aula06/06ex01.py
@@ -0,0 +1,6 @@
+some = input("Digite algo")
+print("A expressão: ",some,", pode ser um alfanumérico? ",some.isalnum())
+print("A expressão: ",some,", pode ser uma letra ",some.isalpha())
+print("A expressão: ",some,", pode estar em maiúsculo? ",some.isupper())
+print("A expressão: ",some,", pode ser um número? ",some.isnumeric())
+print("A expressão: ",some,", pode ser decimal? ",some.isdecimal())
---
+++ Aulas_python/aula06/aula06a.py
@@ -0,0 +1,5 @@
+n1 = int(input("Digite um valor"))
+n2 = int(input("Digite outro valor"))
+s = n1 + n2
+print("O tipo informado é {}".format(type(n1)))
+print("A soma de {} e {} é igual à: {}".format(n1, n2, s))
