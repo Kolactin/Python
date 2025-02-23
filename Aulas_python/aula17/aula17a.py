6d0798acc73ad80f09f238cc22c1210b28a2a39b
Fabio Ricardo Reis da Silva Junior Junior <fabjuniorcc@gmail.com>
2025-02-23 12:19:10 -0300

    aula 17

---
+++ Aulas_python/aula17/aula17a.py
@@ -0,0 +1,18 @@
+# TEMA DA AULA - LISTAS
+
+num = [1, 2, 3, 4]
+
+num.insert(0,0)
+
+del num[2] # num.pop(pode ter indice ou s/ indice apaga o último) || num.remove(5) apaga o valor 5
+
+num.append(5)
+
+print(num)
+
+valores = list(range(4,11)) #Para no elemente 10
+
+valor2 = [0,2,3,76,1,6]
+valor2.sort()
+
+print(valores, valor2)
